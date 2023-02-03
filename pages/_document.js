f751d53f349eaf5eb3c7ba23aae8007cb1861b6a
Brett Carney <brett@brettcarney.com>
2023-02-03 09:05:27 -0600

    Updating meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: `Brett Carney's Blog`,
+    description: `A place to find Development ideas, music, and interesting projects I've created.`,
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
